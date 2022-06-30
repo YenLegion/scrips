5468166a4cb4a9f6e88ad4a559375527e67af56b
YenLegion <petermacchapman@gmail.com>
2022-06-30 00:10:50 -0600

    first

---
+++ Game-Time.ps1
@@ -0,0 +1,14 @@
+# Self-elevate the script if required
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+ if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+  $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+  Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+  Exit
+ }
+}
+
+taskkill /F /T /IM OneDrive.exe 
+
+taskkill /F /T /IM GoogleDriveFS.exe
+
+a2096f00-2862-49e0-9dcc-028216e77bf4
