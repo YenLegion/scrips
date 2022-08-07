c3d0a90bb4766a7b243d01da33434c5cd87e869f
yenlegion <petermacchapman@gmail.com>
2022-08-07 02:32:54 +0000

    color support script

---
+++ check-term-compat.sh
@@ -0,0 +1,11 @@
+if autoload -U is-at-least && is-at-least 5.7; then
+  echo "ZSH $ZSH_VERSION: supports true color"
+else
+  echo "ZSH $ZSH_VERSION: does not support true color"
+fi
+
+if [[ $COLORTERM == (24bit|truecolor) || ${terminfo[colors]} -eq 16777216 ]]; then
+  echo "Terminal supports true color"
+else
+  echo "Terminal does not support true color"
+fi
